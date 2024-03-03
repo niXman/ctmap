786d2a90389a00b4c717e6834bac963dfca6b4b1
niXman <i.nixman@autistici.org>
2024-03-03 15:53:41 +0200

    initial commmit

---
+++ include/ctmap/ctmap.hpp
@@ -0,0 +1,244 @@
+
+// ----------------------------------------------------------------------------
+// MIT License
+//
+// Copyright (c) 2024-2024 niXman (github dot nixman at pm dot me)
+// This file is part of ctmap(github.com/niXman/ctmap) project.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#ifndef __CTMAP__CTMAP_HPP
+#define __CTMAP__CTMAP_HPP
+
+#include <cstdint>
+#include <functional>
+#include <iterator>
+#include <stdexcept>
+
+/*************************************************************************************************/
+
+namespace ctmap {
+namespace details {
+
+/*************************************************************************************************/
+
+template<std::size_t N, typename T, typename CmpLess = std::less<T>>
+struct sorted_vector {
+private:
+    template<std::size_t...>
+    struct integral_holder
+    {};
+
+    template<std::size_t A, std::size_t B, std::size_t... Accum>
+    struct integral_range_: integral_range_<A+1, B, Accum..., A>
+    {};
+
+    template<std::size_t A, std::size_t... Accum>
+    struct integral_range_<A, A, Accum...> {
+        using type = integral_holder<Accum...>;
+    };
+
+    template<std::size_t A, std::size_t B>
+    using integral_range = typename integral_range_<A, B>::type;
+
+    template<typename U>
+    static constexpr U mymin(U i) { return i; }
+    template<typename U, typename... UU>
+    static constexpr U mymin(U i, UU... tt) { return CmpLess{}(i, mymin(tt...)) ? i : mymin(tt...); }
+
+    const T data[N];
+
+    template<std::size_t... II, typename... U>
+    constexpr sorted_vector(integral_holder<II...> ii, T minval, T a, U... tt)
+        :data{
+             (a == minval ? a : sorted_vector<N, T, CmpLess>(ii, minval, tt..., a).data[0])
+            ,(a == minval ? sorted_vector<N-1, T, CmpLess>(tt...)[II] : sorted_vector<N, T, CmpLess>(ii, minval, tt..., a).data[II+1])...
+        }
+    {}
+
+public:
+    using cmp_less = CmpLess;
+
+    template<typename... U>
+    constexpr sorted_vector(U... u)
+        :sorted_vector(integral_range<0, sizeof...(u)-1>(), mymin(u...), u...)
+    {}
+
+    constexpr auto  size()  const noexcept { return N; }
+    constexpr auto* begin() const noexcept { return std::begin(data); }
+    constexpr auto* end  () const noexcept { return std::end  (data); }
+
+    constexpr auto& operator[](std::size_t i) const noexcept { return data[i]; }
+};
+
+template<typename T, typename CmpLess>
+struct sorted_vector<1, T, CmpLess> {
+private:
+    const T data[1];
+
+public:
+    using cmp_less = CmpLess;
+
+    constexpr sorted_vector(T x)
+        :data{x}
+    {}
+
+    constexpr auto  size()  const noexcept { return 1; }
+    constexpr auto* begin() const noexcept { return std::begin(data); }
+    constexpr auto* end  () const noexcept { return std::end  (data); }
+
+    constexpr auto& operator[](std::size_t i) const noexcept { return data[i]; }
+};
+
+template<typename T, typename CmpLess>
+struct sorted_vector<0, T, CmpLess> {
+private:
+
+public:
+    using cmp_less = CmpLess;
+
+    constexpr sorted_vector(T x)
+    {}
+
+    constexpr auto  size()  const noexcept { return 0; }
+    constexpr auto* begin() const noexcept { return nullptr; }
+    constexpr auto* end  () const noexcept { return nullptr; }
+
+    constexpr auto& operator[](std::size_t /*i*/) const noexcept { throw std::invalid_argument("zero size vector!"); }
+};
+
+/*************************************************************************************************/
+
+template<typename T, typename ...Ts>
+constexpr auto make_sorted_vector(T && t, Ts && ...ts) {
+    return details::sorted_vector<1 + sizeof...(Ts), T>(
+        std::forward<T>(t), std::forward<Ts>(ts)...);
+}
+
+template<typename CmpLess, typename T, typename ...Ts>
+constexpr auto make_sorted_vector_cmp(CmpLess, T && t, Ts && ...ts) {
+    return details::sorted_vector<1 + sizeof...(Ts), T, CmpLess>(
+        std::forward<T>(t), std::forward<Ts>(ts)...);
+}
+
+/*************************************************************************************************/
+
+} // ns details
+
+/*************************************************************************************************/
+
+template<typename T>
+using optional_t = std::pair<bool, T>;
+
+template<
+    std::size_t N
+    ,typename K
+    ,typename V
+    ,typename CmpLess = std::less<std::pair<const K, V>>
+    ,typename Storage = details::sorted_vector<N, std::pair<K, V>, CmpLess>
+    >
+struct map {
+    template<typename... Ts>
+    constexpr map(Ts && ...ts)
+        :vec{std::forward<Ts>(ts)...}
+    {}
+
+    constexpr auto* begin() const noexcept { return vec.begin(); }
+    constexpr auto* end  () const noexcept { return vec.end  (); }
+    constexpr auto  size () const noexcept { return vec.size();  }
+
+    constexpr optional_t<V> find(const K &k) const noexcept { return lower_bound(k); }
+    constexpr bool contains(const K &k)      const noexcept { return lower_bound(k).first; }
+
+    constexpr auto& operator[](std::size_t i) const noexcept { return vec[i]; }
+
+private:
+    constexpr optional_t<V> lower_bound(const K &k) const noexcept {
+        const auto *beg = vec.begin();
+        const auto *end = vec.end();
+        std::size_t count = N;
+
+        while ( count > 0 ) {
+            if ( (beg+count/2)->first < k ) {
+                beg = beg+count/2+1;
+                count -= count/2+1;
+            } else {
+                count = count/2;
+            }
+        }
+        if ( beg != end ) {
+            return {(beg->first == k), beg->second};
+        }
+        return {false, V{}};
+    }
+
+private:
+    Storage vec;
+};
+
+template<
+     typename K
+    ,typename V
+    ,typename CmpLess
+    ,typename Storage
+>
+struct map<1, K, V, CmpLess, Storage> {
+    template<typename... Ts>
+    constexpr map(Ts && ...ts)
+        :vec{std::forward<Ts>(ts)...}
+    {}
+
+    constexpr auto& operator[](std::size_t i) const noexcept { return vec[i]; }
+
+    constexpr auto* begin() const noexcept { return vec.begin(); }
+    constexpr auto* end  () const noexcept { return vec.end  (); }
+    constexpr auto  size () const noexcept { return vec.size (); }
+
+    constexpr optional_t<V> find(const K &k) const noexcept {
+        return vec[0].first == k
+            ? optional_t<V>{true, vec[0].second}
+            : optional_t<V>{false, V{}}
+        ;
+    }
+    constexpr bool contains(const K &k) const noexcept { return find(k).first; }
+
+private:
+    Storage vec;
+};
+
+/***********************************************************************************/
+
+template<typename K, typename V, template<typename, typename> class ...Pairs>
+constexpr auto make_map(Pairs<K, V> && ...ts) {
+    return map<sizeof...(Pairs), K, V, std::less<std::pair<K, V>>>(std::forward<Pairs<K, V>>(ts)...);
+}
+
+template<typename CmpLess, typename K, typename V, template<typename, typename> class ...Pairs>
+constexpr auto make_map_cmp(CmpLess, Pairs<K, V> && ...ts) {
+    return map<sizeof...(Pairs), K, V, CmpLess>(std::forward<Pairs<K, V>>(ts)...);
+}
+
+/*************************************************************************************************/
+
+} // ns ctmap
+
+/*************************************************************************************************/
+
+#endif // __CTMAP__CTMAP_HPP
---
+++ test/main.cpp
@@ -0,0 +1,202 @@
+
+// ----------------------------------------------------------------------------
+// MIT License
+//
+// Copyright (c) 2024-2024 niXman (github dot nixman at pm dot me)
+// This file is part of ctmap(github.com/niXman/ctmap) project.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// ----------------------------------------------------------------------------
+
+#include <ctmap/ctmap.hpp>
+
+#include <iostream>
+#include <cassert>
+
+#ifdef NDEBUG
+#   error "This file MUST be compiled with NDEBUG undefined!"
+#endif
+
+template<typename T>
+struct pair_cmp_less {
+    constexpr bool operator() (const T &l, const T &r)
+        const noexcept(noexcept(l.first < r.first))
+    { return l.first < r.first; }
+};
+
+int func(int v) { return v; }
+
+/***********************************************************************************/
+
+int main(int argc, char **) {
+    {
+        constexpr auto vec = ctmap::details::make_sorted_vector(3, 6, 1, 8);
+        static_assert(vec.size() == 4, "");
+
+        static_assert(vec[0] == 1, "");
+        static_assert(vec[1] == 3, "");
+        static_assert(vec[2] == 6, "");
+        static_assert(vec[3] == 8, "");
+    }
+    {
+        using elem_type = std::pair<int, int>;
+        using cmp_less = pair_cmp_less<elem_type>;
+
+        constexpr auto vec = ctmap::details::make_sorted_vector_cmp(
+             cmp_less{}
+            ,std::make_pair(3, 1)
+            ,std::make_pair(6, 2)
+            ,std::make_pair(1, 0)
+            ,std::make_pair(8, 3)
+        );
+        static_assert(vec.size() == 4, "");
+
+        static_assert(vec[0].first == 1, "");
+        static_assert(vec[0].second == 0, "");
+
+        static_assert(vec[1].first == 3, "");
+        static_assert(vec[1].second == 1, "");
+
+        static_assert(vec[2].first == 6, "");
+        static_assert(vec[2].second == 2, "");
+
+        static_assert(vec[3].first == 8, "");
+        static_assert(vec[3].second == 3, "");
+    }
+
+    {
+        using elem_type = std::pair<int, int>;
+        using cmp_less = pair_cmp_less<elem_type>;
+
+        constexpr auto m = ctmap::make_map_cmp(
+             cmp_less{}
+            ,std::make_pair(1,0)
+            ,std::make_pair(4,1)
+            ,std::make_pair(2,2)
+            ,std::make_pair(5,3)
+            ,std::make_pair(3,4)
+        );
+
+        constexpr auto r0 = m.find(1);
+        static_assert(r0.first == true, "");
+        static_assert(r0.second == 0, "");
+
+        constexpr auto r1 = m.find(4);
+        static_assert(r1.first == true, "");
+        static_assert(r1.second == 1, "");
+
+        constexpr auto r2 = m.find(2);
+        static_assert(r2.first == true, "");
+        static_assert(r2.second == 2, "");
+
+        constexpr auto r3 = m.find(5);
+        static_assert(r3.first == true, "");
+        static_assert(r3.second == 3, "");
+
+        constexpr auto r4 = m.find(3);
+        static_assert(r4.first == true, "");
+        static_assert(r4.second == 4, "");
+
+        constexpr auto r5 = m.find(6);
+        static_assert(r5.first == false, "");
+        static_assert(r5.second == 0, "");
+    }
+    {
+        using elem_type = std::pair<int, int>;
+        using cmp_less = pair_cmp_less<elem_type>;
+
+        constexpr auto m = ctmap::make_map_cmp(
+             cmp_less{}
+            ,std::make_pair(1, 0)
+        );
+
+        constexpr auto r0 = m.find(1);
+        static_assert(r0.first == true, "");
+        static_assert(r0.second == 0, "");
+
+        constexpr auto r5 = m.find(6);
+        static_assert(r5.first == false, "");
+        static_assert(r5.second == 0, "");
+    }
+    {
+        using elem_type = std::pair<int, int>;
+        using cmp_less = pair_cmp_less<elem_type>;
+
+        constexpr auto m0 = ctmap::make_map_cmp(
+             cmp_less{}
+            ,std::make_pair(1, 0)
+            ,std::make_pair(4, 1)
+            ,std::make_pair(2, 2)
+            ,std::make_pair(5, 3)
+            ,std::make_pair(3, 4)
+        );
+
+        static_assert(m0[0].first == 1 && m0[0].second == 0, "");
+        static_assert(m0[1].first == 2 && m0[1].second == 2, "");
+        static_assert(m0[2].first == 3 && m0[2].second == 4, "");
+        static_assert(m0[3].first == 4 && m0[3].second == 1, "");
+        static_assert(m0[4].first == 5 && m0[4].second == 3, "");
+
+        std::cout << "1:" << std::endl;
+        for ( const auto &it: m0 )
+            std::cout << it.first << " -> " << it.second << std::endl;
+
+        std::tuple<int, char, long, short, unsigned> t{1, 2, 3, 4, 5};
+        long res = 0;
+        switch ( argc ) {
+            case m0[0].first: res = std::get<m0[0].second>(t); break;
+            case m0[1].first: res = std::get<m0[1].second>(t); break;
+            case m0[2].first: res = std::get<m0[2].second>(t); break;
+            case m0[3].first: res = std::get<m0[3].second>(t); break;
+            case m0[4].first: res = std::get<m0[4].second>(t); break;
+        }
+        std::cout << "argc=" << argc << ", res=" << res << std::endl;
+
+        switch ( argc ) {
+            case 1: {assert(res == 1); break;}
+            case 2: {assert(res == 3); break;}
+            case 3: {assert(res == 5); break;}
+            case 4: {assert(res == 2); break;}
+            case 5: {assert(res == 4); break;}
+            default: { assert(!"wrong number of cmdline args"); }
+        }
+    }
+    {
+        using elem_type = std::pair<int, int(*)(int)>;
+        using cmp_less = pair_cmp_less<elem_type>;
+
+        constexpr auto m0 = ctmap::make_map_cmp(
+             cmp_less{}
+            ,std::make_pair(1, func)
+            ,std::make_pair(4, func)
+            ,std::make_pair(2, func)
+            ,std::make_pair(5, func)
+            ,std::make_pair(3, func)
+        );
+
+        std::cout << "2:" << std::endl;
+        for ( const auto &it: m0 ) {
+            std::cout << it.first << " -> " << it.second(it.first) << std::endl;
+        }
+    }
+
+    return 0;
+}
+
+/***********************************************************************************/
